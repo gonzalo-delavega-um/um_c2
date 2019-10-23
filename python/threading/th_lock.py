6bd316add6a89d7bfb270e92170316f3c4863bbf
d1cor <cordobadie@gmail.com>
2019-10-23 04:37:36 -0300

    fixing issue

--- python/threading/th_lock.py
+++ python/threading/th_lock.py
@@ -1,8 +1,3 @@
-
-<<<<<<< HEAD
-=======
-from multiprocessing import Process, Lock
->>>>>>> 3ead036746115a6d35af794fa5e58b401cea114e
 import threading
 import time
 
@@ -13,7 +8,6 @@ def f(l, n):
         time.sleep(1)
     l.release()
 
-<<<<<<< HEAD
 
 def f_with(l,n):
     """Ejemplo con with, se llama al acquire cuando se entra al bloque, y al release cuando se sale"""
@@ -22,16 +16,11 @@ def f_with(l,n):
             print ("hello world %d %d" % (n,i))
             time.sleep(1)
 
-=======
->>>>>>> 3ead036746115a6d35af794fa5e58b401cea114e
 if __name__ == '__main__':
     lock = threading.Lock()
 
     for num in range(2):
         threading.Thread(target=f, args=(lock, num)).start()
-<<<<<<< HEAD
 
     for num in range(2):
         threading.Thread(target=f_with, args=(lock, num)).start()
-=======
->>>>>>> 3ead036746115a6d35af794fa5e58b401cea114e
