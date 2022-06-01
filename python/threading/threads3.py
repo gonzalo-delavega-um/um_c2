109a17a450d76825ce3e0e3f46635440ccd4a329
d1cor <cordobadie@gmail.com>
2022-06-01 11:48:20 -0300

    thread3

--- python/threading/threads3.py
+++ python/threading/threads3.py
@@ -4,11 +4,11 @@ import time
 import os
 
 
-DELAY=1
+DELAY=3
 
 def thread_function(name):
     print("HILO %d: Este es el hilo: %s (%d)" % (name, threading.current_thread().name, threading.current_thread().ident))
-    time.sleep(DELAY)
+    time.sleep(name)
     print("HILO %d: terminando" % name)
 
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print("MAIN: Iniciando programa principal, antes de crear el thread_______ mi PID es %d (%d)" % (os.getpid(), threading.current_thread().ident))
 
     x1 = threading.Thread(target=thread_function, args=(1,), daemon=True) 
-    x2 = threading.Thread(target=thread_function, args=(2,), daemon=True) 
+    x2 = threading.Thread(target=thread_function, args=(2,)) 
     
     x1.start()
     x2.start()
@@ -29,8 +29,9 @@ if __name__ == "__main__":
         print("Nombre: ", i.name, "(",i.ident,", Vivo?: ",i.is_alive(),", Daemon?:",i.daemon)
 
     # hilo principal esperando...
+    time.sleep(1.5)
+    print("Estado x1: ", x1.is_alive())
     x1.join()
-    x2.join()
 
     # por acá
     # termina
