649c49549ea9a1d11d11b05c12a6d12599fdd0aa
d1cor <cordobadie@gmail.com>
2022-08-30 20:14:29 -0300

    http

--- python/sockets/http/server_http.py
+++ python/sockets/http/server_http.py
@@ -1,14 +1,36 @@
 import http.server
 import socketserver
 
-PORT = 1234
+# GET / HTTP/1.1
 
+PORT = 1111
+
+
+class handler_manual (http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        print("REQUEST: ", self.requestline)
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write(b'hola mundo GET\n')
+
+    def do_POST(self):
+        print("REQUEST: ", self.requestline)
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write(b'hola mundo POST\n')
 
 socketserver.TCPServer.allow_reuse_address = True
-myhttphandler = http.server.SimpleHTTPRequestHandler
+
+#myhttphandler = http.server.BaseHTTPRequestHandler
+#myhttphandler = http.server.SimpleHTTPRequestHandler
+myhttphandler = handler_manual
 
 
-httpd = socketserver.TCPServer(("", PORT), myhttphandler)
+#httpd = socketserver.TCPServer(("", PORT), myhttphandler)
+httpd = http.server.HTTPServer(("", PORT), myhttphandler)
+#httpd = http.server.ThreadingHTTPServer(("", PORT), myhttphandler)
 
 print(f"Opening httpd server at port {PORT}")
 
