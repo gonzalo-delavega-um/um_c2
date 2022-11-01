b631d2451cf238fa4d2f56de371a5618b4a09030
d1cor <cordobadie@gmail.com>
2022-11-01 00:54:35 -0300

    calc monolitico

--- python/celery/calc_clase/celery_calc_config.py
+++
@@ -1,5 +0,0 @@
-
-from celery import Celery
-
-app = Celery('celery_calc', broker='redis://analytics.juncotic.com:6379', backend='redis://analytics.juncotic.com:6379', include=['celery_calc', 'celery_log'])
-
